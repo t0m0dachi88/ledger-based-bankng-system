848ee2c7a4d0dc98fd62370349b516985f0180b3
irfan <76irfansadik@gmail.com>
2026-05-06 23:49:27 +0600

    setup done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,24 +6,7 @@ const jwt=require('jsonwebtoken');
 async function register(req,res)
 {
     const {email,password,name}=req.body
-    
-    try {
-        const isUserAvailable=await userModel.findOne({email})
-        if(isUserAvailable)
-        {
-            return res.status(400).json({message: "user exists"})
-        }
-
-        const hash=await bcrypt.hash(password,10)
-        const newUser=await userModel.create({email,password:hash,name})
-
-        const token=jwt.sign({userID:newUser._id},process.env.JWT_SECRET)
-
-        res.status(200).json({user: newUser, token: token})
-    } catch(error) {
-        res.status(500).json({message: error.message})
-    }
-    
+     
 }
 
 module.exports = { register }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router=require('express').Router();
 const authController=require("../controllers/auth.controller")
 
 
-router.get("/register",authController.register)
+router.post("/register",authController.register)
 
 
 
